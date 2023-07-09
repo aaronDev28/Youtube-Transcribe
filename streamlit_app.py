dd1a015527a462a43396416b82fd6c91a427b4aa
RonBoy <123363908+aaronDev28@users.noreply.github.com>
2023-07-09 14:38:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,45 +4,45 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 
 st.set_page_config(page_title="YouTube Transcription App")
 
-st.title('📺 YouTube Transcription App')
+st.title('📽️ YouTube Transcriber')
 
-# YouTube URL
-yt_url = st.text_input('Enter YouTube video URL', '')
+#Getting the YT video's URL
+youtube_url = st.text_input('Enter YouTube Video URL', '')
 
-# Get YouTube video ID
-def extract_yt_id(input_url):
-  processed_url = []
-  if input_url.startswith('https://www.youtube.com') or input_url.startswith('https://youtube.com'):
-    input_url_split = input_url.split('=')[-1]
-    processed_url.append(input_url_split)
-  if input_url.startswith('https://youtu.be/'):
-    input_url_split = input_url.split('/')[-1]
-    processed_url.append(input_url_split)
-  return processed_url
+# Getting the YT video's ID
+def extract_youtubeID(inputURL):
+  processedURL = []
+  if inputURL.startswith('https://www.youtube.com') or inputURL.startswith('https://youtube.com'):
+    inputURL_split = inputURL.split('=')[-1]
+    processedURL.append(inputURL_split)
+  if inputURL.startswith('https://youtu.be/'):
+    inputURL_split = inputURL.split('/')[-1]
+    processedURL.append(inputURL_split)
+  return processedURL
 
-# Get YouTube thumbnail image
-def get_yt_img(input_id):
+# Getting the YT video's Thumbnail Picture
+def get_thumbnail(input_id):
   return st.image(f'http://i.ytimg.com/vi/{input_id[0]}/maxresdefault.jpg', width=350)
 
-# Retrieve transcript from YouTube video
-def get_transcript(input_id):
-  input_url = f'https://www.youtube.com/watch?v={input_id[0]}'
-  loader = YoutubeLoader.from_youtube_url(input_url , add_video_info=False)
-  results = loader.load()
-  yt_text = results[0].page_content
-  return st.write(yt_text)
+# Retrieve the YT video's transcription
+def getTranscription(input_id):
+  inputURL = f'https://www.youtube.com/watch?v={input_id[0]}'
+  loaderObj = YoutubeLoader.from_youtube_url(inputURL , add_video_info=False)
+  loadResult = loaderObj.load()
+  ytText = loadResult[0].page_content
+  return st.write(ytText)
   
-# Conditional display of content
-if yt_url == '':
+# Interface
+if youtube_url == '':
   st.warning('👆 Please enter a YouTube video URL to get started!')
   with st.expander('See example URL'):
     st.code('https://www.youtube.com/watch?v=TX9qSaGXFyg')
     st.code('https://www.youtube.com/watch?v=I3cjbB38Z4A')
 else: 
   ## Display YouTube thumbnail image
-  yt_id = extract_yt_id(yt_url)
-  get_yt_img(yt_id)
+  youtubeID = extract_youtubeID(youtube_url)
+  get_thumbnail(youtubeID)
   
   ## Display transcription
-  with st.expander('See video transcript'):
-    get_transcript(yt_id)
+  with st.expander('See Video Transcript'):
+    getTranscription(youtubeID)
